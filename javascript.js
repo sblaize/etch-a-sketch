e904bfb42a0979de57cf34c60c14715f65e3c261
sblaize <blaizesekani@gmail.com>
2023-01-25 20:31:18 -0500

    Removed innerText and added mouseover

--- javascript.js
+++ javascript.js
@@ -1,10 +1,15 @@
 const container = document.getElementById('container');
 
+
+
 function makeRows(rows, cols) {
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
     for (c = 0; c < (rows * cols); c++) {
         let cell = document.createElement('div');
+        cell.addEventListener("mouseover", () => {
+            cell.style.backgroundColor = "blue"
+        });
         container.appendChild(cell).className = "grid-item";
     };
 };
