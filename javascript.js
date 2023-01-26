b997841fe5281947155e11220b6f141465bbc00a
sblaize <blaizesekani@gmail.com>
2023-01-25 19:05:37 -0500

    Changed grid-rows to grid-cols in line 5

--- javascript.js
+++ javascript.js
@@ -2,8 +2,8 @@ const container = document.getElementById('container');
 
 function makeRows(rows, cols) {
     container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-rows', cols);
-    for (c=0; c < (rows * cols); c++) {
+    container.style.setProperty('--grid-cols', cols);
+    for (c = 0; c < (rows * cols); c++) {
         let cell = document.createElement('div');
         cell.innerText = (c+1);
         container.appendChild(cell).className = "grid-item";
