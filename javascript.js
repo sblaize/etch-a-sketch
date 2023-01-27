e08c69762ddcf3c7e7253e401527f897f4373da3
sblaize <blaizesekani@gmail.com>
2023-01-26 20:50:56 -0500

    Added addElement function

--- javascript.js
+++ javascript.js
@@ -1,7 +1,5 @@
 const container = document.getElementById('container');
 
-
-
 function makeGrid(rows, cols) {
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
@@ -14,4 +12,21 @@ function makeGrid(rows, cols) {
     };
 };
 
-makeGrid(16,16);
+let input = document.getElementsByTagName('input');
+let button = document.getElementsByTagName('button');
+
+
+document.body.onload = addElement;
+
+function addElement() {
+const newDiv = document.createElement('div');
+newDiv.setAttribute = ('id', 'newDiv')
+const button = document.createElement('button');
+const input = document.createElement('input');
+button.textContent = "Enter Number Here"
+newDiv.appendChild(button).className = "button";
+newDiv.appendChild(input).className = "input";
+document.body.insertBefore(newDiv, container);
+}
+
+
